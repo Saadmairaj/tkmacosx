03b78e511367405add2d227a69cde3359e31a35d
Saad Mairaj <46227224+Saadmairaj@users.noreply.github.com>
2019-06-09 15:48:56 +0530

    Update __init__.py

--- tkmacosx/__init__.py
+++ tkmacosx/__init__.py
@@ -1,11 +1,17 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+"""
+This module provides some modified widgets of Tkinter which works better on macos
+and some more useful functions and classes as well. For example Button of tkmacosx which 
+looks and feels exaclty like a native tkinter button can change its background 
+and foreground colors.
 
-from basewidget import *
-from variables import *
-from widget import *
-from colors import *
+Read more about tkmacosx in detail on
+https://github.com/Saadmairaj/tkmacosx/tree/master/tkmacosx.
+"""
+
+from tkmacosx.basewidget import check_appearence, get_shade
+from tkmacosx.variables import ColorVar, DictVar, demo_colorvar
+from tkmacosx.widget import Button, SFrame, demo_sframe, demo_button
+from tkmacosx.colors import Hex, OrderedHex
 
 if __name__ == "__main__":
     demo_sframe()
