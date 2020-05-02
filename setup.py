55b29e100513df4f7cb17fabb3e3169a06ca9384
Saad <46227224+Saadmairaj@users.noreply.github.com>
2020-05-02 10:32:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def long_description():
 
 setup(
     name="tkmacosx",
-    version="0.0.9",
+    version="0.1.0",
     description="Tkmacosx is a Python library extension to the Tkinter module for macOS",
     long_description=long_description(),
     long_description_content_type="text/markdown",
@@ -18,17 +18,28 @@ setup(
     author_email="Saadmairaj@yahoo.in",
     license="Apache",
     classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Build Tools",
         'Topic :: Software Development :: Widget Sets',
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Environment :: MacOS X"
+        "Environment :: MacOS X",
+
     ],
     keywords=['tkinter', 'macos', 'variable', 'widgets', 'colorscale', 'tk'],
     packages=["tkmacosx"],
     include_package_data=True,
-    install_requires=['colour', 'pillow']
+    install_requires=['colour', 'pillow'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/Saadmairaj/tkmacosx',
+        'Source': 'https://github.com/Saadmairaj/tkmacosx/issues',
+    },
 )
